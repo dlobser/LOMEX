428ceeb7d0fb2cd30e24025d0c2cbdc969ff3a26
Laura Chen <jhclaura@gmail.com>
2016-10-27 16:34:30 -0400

    update to 6 Indicators and scenes

--- Assets/Dev/DL/Scripts/copyPosition.cs
+++ Assets/Dev/DL/Scripts/copyPosition.cs
@@ -4,35 +4,16 @@ using System.Collections.Generic;
 
 public class CopyPosition : MonoBehaviour {
 
-	public GameObject[] target;
-	public List<GameObject> toBePlaced = new List<GameObject>();
-	int toBePlacedCount;
+	public GameObject target;
 
 	// Use this for initialization
 	void Start () {
-		foreach (Transform child in transform) {
-			toBePlaced.Add (child.gameObject);
-		}
-		toBePlacedCount = toBePlaced.Count;
+
 	}
 	
 	// Update is called once per frame
-//	void Update () {
-//		if (this.transform.position != target.transform.position)
-//			this.transform.position = target.transform.position;
-//	}
-
-	public void UpdateTargetTransform(int targetIndex, int whichToUpdate){
-		if (whichToUpdate == 0)
-		{
-			toBePlaced[ targetIndex % toBePlacedCount ].transform.position = target [targetIndex].transform.position;
-		} else if (whichToUpdate == 1)
-		{
-			toBePlaced[ targetIndex % toBePlacedCount ].transform.rotation = target [targetIndex].transform.rotation;
-		} else
-		{
-			toBePlaced[ targetIndex % toBePlacedCount ].transform.position = target [targetIndex].transform.position;
-			toBePlaced[ targetIndex % toBePlacedCount ].transform.rotation = target [targetIndex].transform.rotation;
-		}
+	void Update () {
+		if (this.transform.position != target.transform.position)
+			this.transform.position = target.transform.position;
 	}
 }
---
+++ Assets/Dev/LC/Scripts/CopyPosition_L.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CopyPosition_L : MonoBehaviour {
+
+	public GameObject[] target;
+	public GameObject[] toBePlaced;
+	int toBePlacedCount;
+
+	// Use this for initialization
+	void Start () {
+		toBePlacedCount = toBePlaced.Length;
+	}
+
+	public void UpdateTargetTransform(int targetIndex, int whichToUpdate){
+		if (whichToUpdate == 0)
+		{
+			toBePlaced[ targetIndex % toBePlacedCount ].transform.position = target [targetIndex].transform.position;
+		} else if (whichToUpdate == 1)
+		{
+			toBePlaced[ targetIndex % toBePlacedCount ].transform.rotation = target [targetIndex].transform.rotation;
+		} else
+		{
+			toBePlaced[ targetIndex % toBePlacedCount ].transform.position = target [targetIndex].transform.position;
+			toBePlaced[ targetIndex % toBePlacedCount ].transform.rotation = target [targetIndex].transform.rotation;
+		}
+	}
+
+	public void HideTarget(int targetIndex){
+		if (toBePlaced [targetIndex].activeInHierarchy) {
+			target [targetIndex].SetActive (false);
+			toBePlaced [targetIndex].SetActive (false);
+		}
+			
+	}
+
+	public void ShowTarget(int targetIndex){
+		if (!toBePlaced [targetIndex].activeInHierarchy) {
+			target [targetIndex].SetActive (true);
+			toBePlaced [targetIndex].SetActive (true);
+		}
+			
+	}
+}
