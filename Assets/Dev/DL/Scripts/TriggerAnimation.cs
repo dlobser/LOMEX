a5a75afd6745198dcc3aa5c6538d4648ee2c6a26
david lobser <davidlobser@yahoo.com>
2016-10-27 17:01:42 -0400

    animation work

---
+++ Assets/Dev/DL/Scripts/TriggerAnimation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimation : MonoBehaviour {
+
+	public string triggerName;
+	public Animator anim;
+
+	public void Trigger(){
+		if(anim!=null)
+			anim.SetTrigger (triggerName);
+	}
+}
